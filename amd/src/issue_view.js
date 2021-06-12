7db9b48db668d1192a9e73c5d7f56dc46e727a38
David Rise Knotten <david_knotten@hotmail.no>
2021-06-12 15:05:14 +0200

    Refactored out requirejs from issue view

--- amd/src/issue_view.js
+++ amd/src/issue_view.js
@@ -1,19 +1,18 @@
 
-define(['jquery'], function($) {
-    class IssueView {
-        constructor(closeissuestring, commentandcloseissuestring) {
-            const commentandcommitbtn = $("button[name=closeissue]").get(0);
-            const commentEditor = $("#commenteditor").get(0);
-            commentEditor.addEventListener("change", event => {
-                if (commentEditor.value == ('<p dir="ltr" style="text-align: left;"><br></p>')) {
-                    commentandcommitbtn.innerText = closeissuestring;
-                } else {
-                    commentandcommitbtn.innerText = commentandcloseissuestring;
-                }
-            })
-        }
+import $ from 'jquery';
+
+class IssueView {
+    constructor(closeissuestring, commentandcloseissuestring) {
+        const commentandcommitbtn = $("button[name=closeissue]").get(0);
+        const commentEditor = $("#commenteditor").get(0);
+        commentEditor.addEventListener("change", event => {
+            if (commentEditor.value == ('<p dir="ltr" style="text-align: left;"><br></p>')) {
+                commentandcommitbtn.innerText = closeissuestring;
+            } else {
+                commentandcommitbtn.innerText = commentandcloseissuestring;
+            }
+        })
     }
+}
 
-    return IssueView;
-
-})
+export default IssueView;
