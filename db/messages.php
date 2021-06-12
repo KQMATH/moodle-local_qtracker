886e668a76a266ef5e22043543165b23d8fd1924
David Rise Knotten <david_knotten@hotmail.no>
2021-06-12 13:24:51 +0200

    Quick push

--- db/messages.php
+++ db/messages.php
@@ -1,10 +1,11 @@
 <?php
+defined('MOODLE_INTERNAL') || die();
+
 
 $messageproviders = array (
     'issueresponse' => array (
         'default' => array (
-            'popup' => MESSAGE_PERMITTED + MESSAGE_DEFAULT_LOGGEDIN,
-            'email' => MESSAGE_DISALLOWED
+            'popup' => MESSAGE_PERMITTED + MESSAGE_DEFAULT_LOGGEDOFF + MESSAGE_DEFAULT_LOGGEDIN
         )
     )
 );
