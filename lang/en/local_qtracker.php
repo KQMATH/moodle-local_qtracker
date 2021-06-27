576f9c4cc27025ddacad6597cbc2bd7dddcd3ddb
David <david_knotten@hotmail.no>
2021-06-27 17:48:46 +0200

    aside block prototype

--- lang/en/local_qtracker.php
+++ lang/en/local_qtracker.php
@@ -75,6 +75,7 @@ $string['openedissueon'] = 'opened issue on ';
 $string['name'] = 'Name';
 
 $string['tags'] = 'Tags';
+$string['linkedissues'] = 'Linked issues';
 
 $string['new'] = 'New';
 $string['open'] = 'Open';
